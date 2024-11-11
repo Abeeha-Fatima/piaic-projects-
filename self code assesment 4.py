68b52dac45e088adefb42edfae95d053069c1be9
Abeeha-Fatima <syedaabeehafatima27@gmail.com>
2024-11-11 18:41:43 +0500

    age riddle

---
+++ self code assesment 4.py
@@ -0,0 +1,14 @@
+# age riddle
+def main():
+    anton :int = 21
+    beth :int = anton +6
+    chen :int = beth + 20
+    drew :int = chen + anton
+    ethan :int = chen 
+    print("anton is "+ str(anton)) 
+    print("beth is "+ str (beth))
+    print("chen is "+str (chen)) 
+    print("drew is"+str (drew) )
+    print("ethan is"+ str(ethan)) 
+if __name__ =='__main__':
+    main()
